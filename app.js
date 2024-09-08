8a0778cd924fd867198a0010ad2f74583ffe580d
JarlTheGamer <103575351+JarlTheGamer@users.noreply.github.com>
2024-09-08 17:09:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
-import nodemailer from 'nodemailer';
+const nodemailer = require("nodemailer");
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { name, email, message } = req.body;
+export default async (req, res) => {
 
-    // Configure Nodemailer
-    const transporter = nodemailer.createTransport({
-      service: 'gmail', // or another email provider
-      auth: {
-        user: 'frederickyxyzsubmissions@gmail.com', // Your email from environment variables
-        pass: 'Jarl@200820', // Your email password from environment variables
-      },
+const { firstName, lastName, email, message } = JSON.parse(req.body);
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: "frederickyxyzsubmissions@gmail.com",
+        pass: "Jarl@200820",
+    },
+    secure: true,
+});
+
+await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
     });
+});
 
-    const mailOptions = {
-      from: email,
-      to: 'frederickyxyz@gmail.com', // Your private work email
-      subject: `Form submission from ${name}`,
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    };
+const mailData = {
+    replyTo: email,
+    to: "frederickyxyz@gmail.com",
+    subject: `form message`,
+    text: message,
+    html: `${message}`,
+};
 
-    try {
-      await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: 'Email sent successfully!' });
-    } catch (error) {
-      res.status(500).json({ message: 'Error sending email.' });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    });
+});
+
+res.status(200).json({ status: "OK" });
+};
